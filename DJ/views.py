00abce7e03d2562d883ec2480fb21df1132435bd
Surya Teja (Nokia) <surya.teja@nokia.com>
2023-07-19 07:24:06 +0530

    Basic Django Project

--- DJ/views.py
+++ DJ/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from employees.apps import EmployeesConfig
+from employees.models import Employee
+
 def home(request):
-    return render(request, 'home.html')
+    """Home Method"""
+    employees = Employee.objects.all()
+    context = {
+        'employees' : employees
+    }
+    return render(request, 'home.html', context)
--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from employees.models import Employee
 
 # Create your views here.
+
+
+def employee_details(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    context={
+        'employee' : employee
+    }
+    return render(request, 'employee_detail.html', context)
